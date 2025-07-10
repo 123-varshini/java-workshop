ef23f4259f8eddc01c6c7d94a2a502cc521d649d
123-varshini <varshinijothi17@gmail.com>
2025-07-10 14:26:53 +0530

    listexamples.java

---
+++ src/main/java/com/vetias/java/workshop/collection/ListExamples.java
@@ -0,0 +1,16 @@
+package com.vetias.java.workshop.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExamples {
+
+    public static void main(String[]args){
+        List<String> names=new ArrayList<>();
+        names.add("ram");
+        names.add("raja");
+        names.add("ramya");
+        names.add("rahul");
+        System.out.println(names);
+    }
+}
