23e18d624f6b3d248fb3962ff1b124ce212febb6
123-varshini <varshinijothi17@gmail.com>
2025-07-10 14:55:21 +0530

    Average.java

---
+++ src/main/java/com/vetias/java/workshop/basics/Average.java
@@ -0,0 +1,14 @@
+package com.vetias.java.workshop.basics;
+
+
+
+public class Average {
+    public static void main(String[] args) {
+        int a=89,b=76,c=69,d=98,e=58;
+
+        int tot = a+b+c+d+e;
+
+        System.out.println("The total is: " + tot );
+    }
+}
+
