a16a806a78198ae2e6ae5faaf7613a182be14400
123-varshini <varshinijothi17@gmail.com>
2025-07-11 09:18:45 +0530

    Arrays.java

---
+++ src/main/java/com/vetias/java/workshop/basics/Arrays.java
@@ -0,0 +1,13 @@
+package com.vetias.java.workshop.basics;
+
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[]marks={20,30,40,50,60,70};
+        for(int mark:marks){
+            System.out.println(mark);
+        }
+    }
+}
+
+
